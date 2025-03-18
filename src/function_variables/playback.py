b4e9c6124d965059fe7ae4b869a432fc06ca4be7
Lachlan <lachlan.macinnis1@education.nsw.gov.au>
2025-03-18 00:47:29 +0000

    hello

---
+++ src/function_variables/playback.py
@@ -0,0 +1,9 @@
+def playback():
+    user_input = input()
+
+    output = user_input.replace(" ", "...")
+    print(output)
+
+
+if __name__ == "__main__":
+    playback()
---
+++ src/lesson_0/indoor.py
@@ -0,0 +1,8 @@
+def indoor():
+    # This code is an example and should be replaced with your own solution
+    value = input()
+    print(value.lower())
+
+
+if __name__ == "__main__":
+    indoor()
